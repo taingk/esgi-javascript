b49a0c5b9cc172f97159e90091162e576563bee2
Kevin <taingkn@gmail.com>
2018-11-27 14:45:48 +0100

    test

--- exercice-2/type-check.js
+++ exercice-2/type-check.js
@@ -1,8 +1,10 @@
 const type_check_v1 = (check, type) => {
     switch (true) {
-        case check === null && type.toLowerCase() === 'null':
+        case check === null && type === 'null':
             return true;
-        case Array.isArray(check) && type.toLowerCase() === 'array':
+        case Array.isArray(check) && type === 'array':
+            return true;
+        case check === undefined && type === 'undefined':
             return true;
         case typeof check === type && check !== null && !Array.isArray(check):
             return true;
