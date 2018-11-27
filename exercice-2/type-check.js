28ceaeeb0e376a47b573fd3aa4d677761e69040d
Kevin <taingkn@gmail.com>
2018-11-27 15:17:57 +0100

    type check 2 done

--- exercice-2/type-check.js
+++ exercice-2/type-check.js
@@ -1,13 +1,23 @@
 const type_check_v1 = (check, type) => {
     switch (true) {
         case check === null && type === 'null':
-            return true;
         case Array.isArray(check) && type === 'array':
-            return true;
         case check === undefined && type === 'undefined':
-            return true;
         case typeof check === type && check !== null && !Array.isArray(check):
             return true;
+        default:
+            return false;
+    }
+}
+
+const type_check_v2 = (check, type) => {
+    switch (true) {
+        case type.hasOwnProperty('type') && !type.hasOwnProperty('value') && !type.hasOwnProperty('enum'):
+            return type_check_v1(check, type.type);
+        case type.hasOwnProperty('type') && type.hasOwnProperty('value') && !type.hasOwnProperty('enum'):
+            return type_check_v1(check, type.type) && check === type.value;
+        case !type.hasOwnProperty('type') && !type.hasOwnProperty('value') && type.hasOwnProperty('enum'):
+            return check === type.enum.length;
         default:
             return false;
     }
